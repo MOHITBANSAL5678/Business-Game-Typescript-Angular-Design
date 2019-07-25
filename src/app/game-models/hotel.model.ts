039b10bba937e6f3477fdbe69e7e10682172475f
Mohit Bansal <39431535+MOHITBANSAL5678@users.noreply.github.com>
2019-07-25 10:47:24 +0530

    Update hotel.model.ts

--- src/app/game-models/hotel.model.ts
+++ src/app/game-models/hotel.model.ts
@@ -1,13 +1,29 @@
-import { Player } from './player.model';
+import { Categories } from './../static/categories.enum';
+import { Cell } from './cell.model';
 import { HotelGrade } from '../static/hotel-grade.enum';
+import { Player } from './player.model';
 
-export class Hotel {
-    price: number;
+export class Hotel extends Cell {
     owner: Player;
-    type: HotelGrade.G | HotelGrade.P | HotelGrade.S;
+    grade: HotelGrade.Gold | HotelGrade.Silver | HotelGrade.Platinum;
+
+    constructor() {
+        super()
+        this.type = Categories.H;
+        this.grade = HotelGrade.Silver;
+    }
     getRent() {
-        if (this.type === HotelGrade.S) {
+        if (this.type === HotelGrade.Silver) {
             return 100;
         }
+        if (this.type === HotelGrade.Gold) {
+            return 100;
+        }
+        if (this.type === HotelGrade.Platinum) {
+            return 100;
+        }
+
     }
+
+
 }
